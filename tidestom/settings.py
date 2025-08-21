b8e29f2cb027674c7b47046bb04d98065d549dc7
Chris Duffy <c.jduffy@me.com>
2025-08-21 16:24:48 +0100

    Update settings.py

--- tidestom/settings.py
+++ tidestom/settings.py
@@ -184,7 +184,7 @@ DATE_FORMAT = 'Y-m-d'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
                     os.path.join(TEST_DIR, 'sims'),]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
