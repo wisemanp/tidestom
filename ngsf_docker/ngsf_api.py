b637a2fdd2f8ebad005b14274895c46727598af5
Chris Duffy <c.jduffy@me.com>
2025-09-01 23:18:48 +0100

    NGSF Partial Form

--- ngsf_docker/ngsf_api.py
+++ ngsf_docker/ngsf_api.py
@@ -7,7 +7,18 @@ class Params(BaseModel):
     file: str | None = 'sn2003jo.dat'
     z: float | None = 0.0
     z_min: float | None = 0.0
-
+    z_max: float | None = 0.1
+    z_int: float | None = 0.01
+    resolution: float | None = 10
+    lower_lam: float | None = 0.00
+    upper_lam: float | None = 0.0
+    mask_galaxy: bool | None = True
+    mask_telluric: bool | None = True
+    epoch_high: float | None = 0.0
+    epoch_low: float | None = 0.0
+    alam_high: float | None = 2
+    alam_low: float | None = -2
+    alam_interval: float | None = 0.2
 
 app = FastAPI()
 
