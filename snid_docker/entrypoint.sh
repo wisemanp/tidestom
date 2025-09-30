33a746fb1a99ab75b9d8997fabb278e582c00eed
Chris Duffy <c.jduffy@me.com>
2025-09-30 17:05:06 +0100

    Update entrypoint.sh

--- snid_docker/entrypoint.sh
+++ snid_docker/entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -e
+set -ex  # <- print commands
 
-# Fix ownership of mounted volume
+echo "ENTRYPOINT START: user=$(whoami), args=$@"
 chown -R sniduser:snidgroup /snid_api_runs
 mkdir -p /media/snid_template_options
 chown -R sniduser:snidgroup /media/snid_template_options
 
-# Execute the main container command (uvicorn)
 if [ "$#" -eq 0 ]; then
+  echo "No command provided, falling back to uvicorn"
   exec uvicorn run_snid:app --host 0.0.0.0 --port 8000
 else
+  echo "Executing command: $@"
   exec "$@"
 fi
