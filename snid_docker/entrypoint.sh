3869750270dc9159bbbb0ff3cfc287a2a7b3c2dd
Chris Duffy <c.jduffy@me.com>
2025-09-30 16:39:35 +0100

    Update entrypoint.sh

--- snid_docker/entrypoint.sh
+++ snid_docker/entrypoint.sh
@@ -7,6 +7,8 @@ mkdir -p /media/snid_template_options
 chown -R sniduser:snidgroup /media/snid_template_options
 
 # Execute the main container command (uvicorn)
-# exec su -s /bin/bash sniduser -c "$@"
-exec "$@"
-
+if [ "$#" -eq 0 ]; then
+  exec uvicorn run_snid:app --host 0.0.0.0 --port 8000
+else
+  exec "$@"
+fi
