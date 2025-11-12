2cea67ae6fbbc74eeb2c2f6bc6b264e44d70dcfc
Chris Duffy <c.jduffy@me.com>
2025-11-12 14:04:11 +0000

    Update settings.py

--- tidestom/settings.py
+++ tidestom/settings.py
@@ -22,6 +22,7 @@ USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
