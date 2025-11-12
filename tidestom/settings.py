70d296de4db696bd641f8adf76183726ce3d0dcd
Chris Duffy <c.jduffy@me.com>
2025-11-12 14:14:06 +0000

    Update settings.py

--- tidestom/settings.py
+++ tidestom/settings.py
@@ -135,7 +135,7 @@ WSGI_APPLICATION = 'tidestom.wsgi.application'
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': DB_NAME,  # Main database
+        'NAME': 'tidestom',  # Main database
         'USER': USER,
         'PASSWORD': DB_PASS,
         'HOST': DB_HOST,
