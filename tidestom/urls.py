dd12b67bb957efd677d1d29bf1c4258bbdf57a63
Georgios Dimitriadis <georgios.dimitriadis.astro@gmail.com>
2025-11-21 14:42:32 +0000

    download spec
    added download button at spec tab

--- tidestom/urls.py
+++ tidestom/urls.py
@@ -20,7 +20,7 @@ from .views import (
     LatestView, SubmitClassificationView, get_subclasses, MyTargetDetailView
 )
 from custom_code.views import SnidFormAjaxView, NGSFFormAJAXView
-from myplots.views import target_spectroscopy_partial #,download_spectrum_ascii ## georgios ##
+from myplots.views import target_spectroscopy_partial,download_spectrum_ascii ## georgios ##
 urlpatterns = [
     path(
         'about/', TemplateView.as_view(template_name='about.html'),
