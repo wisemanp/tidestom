aa91612cbfa08f02ba996f110df4660c5065e368
Chris Duffy <c.jduffy@me.com>
2025-12-02 16:53:24 +0000

    Local mode changes for CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Waiting for API config file..."
-while [ ! -f /snid_api_runs/snid_template_options/subtypes.txt ]; do
-    sleep 1
-done
+if [[ -v LOCAL_MODE ]]; then
+	echo "Waiting for API config file..."
+	while [ ! -f /snid_api_runs/snid_template_options/subtypes.txt ]; do
+		sleep 1
+	done
 
-echo "Copying subtype options into static..."
-mkdir -p /app/static/config
-cp /snid_api_runs/snid_template_options/subtypes.txt /app/static/config/
+	echo "Copying subtype options into static..."
+	mkdir -p /app/static/config
+	cp /snid_api_runs/snid_template_options/subtypes.txt /app/static/config/
+fi
 
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput
