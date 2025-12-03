6865bf164b2cbf24676c4d956182d2f2d90e8a37
Chris Duffy <c.jduffy@me.com>
2025-12-03 14:48:10 +0000

    Update views.py

--- custom_code/views.py
+++ custom_code/views.py
@@ -145,7 +145,7 @@ class NGSFFormAJAXView(FormView):
             if candidate.exists():
                 p = candidate
 
-        temp_file_path = 'ngsf_api_runs/target.fits'
+        temp_file_path = '/ngsf_api_runs/target.fits'
         shutil.copy2(str(p), temp_file_path)
         form.cleaned_data['spectrum'] = temp_file_path
 
