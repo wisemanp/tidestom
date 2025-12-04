37225c0631159db00ac267c7f0324c5eefa4d6b1
Chris Duffy <c.jduffy@me.com>
2025-12-04 16:51:47 +0000

    Update myplots_tags.py

--- myplots/templatetags/myplots_tags.py
+++ myplots/templatetags/myplots_tags.py
@@ -83,7 +83,7 @@ def target_spectroscopy(context, target, dataproduct=None, snid_path=None, ngsf_
 
     if ngsf_path is not None:
         try:
-            ngsf_file = ngsf_path
+            ngsf_file = "/ngsf_api_runs/workspaces/2787710e6570803c552dab15d0da8a467a20bb13b3c9a0c5815be290fc0ca02f/30958643/run_2025-12-04T16-47-56Z/spectrum.csv"
             fig = add_ngsf_templates(ngsf_file,
                              spectrum.spectral_axis.value,
                              spectrum.flux.value,
