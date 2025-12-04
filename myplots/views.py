a808b89ea7f22f380de0ccb1d4fb3132918a0eef
Chris Duffy <c.jduffy@me.com>
2025-12-04 16:31:58 +0000

    Update views.py

--- myplots/views.py
+++ myplots/views.py
@@ -5,9 +5,11 @@ from custom_code.models import TidesTarget
 
 def target_spectroscopy_partial(request, target_id):
     snid_path = request.GET.get("snid_path")
+    ngsf_path = request.GET.get("ngsf_path")
     target = get_object_or_404(TidesTarget, pk=target_id)
 
     return render(request, "myplots/target_spectroscopy_partial.html", {
         "target": target,
         "snid_path": snid_path,
+        "ngsf_path": ngsf_path,
         })
