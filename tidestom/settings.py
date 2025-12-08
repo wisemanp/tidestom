a2852bcaef6553ea2082c66b97d91eb7b72984a4
Chris Duffy <c.jduffy@me.com>
2025-12-08 16:17:48 +0000

    Update settings.py

--- tidestom/settings.py
+++ tidestom/settings.py
@@ -176,8 +176,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/marshal'
+LOGOUT_REDIRECT_URL = '/marshal'
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
