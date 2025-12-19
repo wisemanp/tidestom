69ec627c147ac0efc224015f890c145ec16bf49c
Chris Duffy <c.jduffy@me.com>
2025-12-19 16:28:42 +0000

    Update spectroscopy_settings.py

--- myplots/templatetags/spectroscopy_settings.py
+++ myplots/templatetags/spectroscopy_settings.py
@@ -146,7 +146,7 @@ def add_snid_templates(pysnid_file: str, obs_wave: np.ndarray, obs_flux: np.ndar
         # match observed grid
         model_wave, model_flux = match_grid(obs_wave, model_wave, model_flux)
 
-        temp_info = snidres.results.iloc[i-1]
+        temp_info = snidres.results.iloc[i]
         fig.add_trace(go.Scatter(
             x=model_wave,
             y=model_flux,
