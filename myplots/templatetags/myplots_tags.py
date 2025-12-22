7812a180503cf24fb4e2a3a10f86e598d88bd715
Chris Duffy <c.jduffy@me.com>
2025-12-22 11:58:29 +0000

    Update myplots_tags.py

--- myplots/templatetags/myplots_tags.py
+++ myplots/templatetags/myplots_tags.py
@@ -82,7 +82,7 @@ def target_spectroscopy(context, target, dataproduct=None, snid_path=None, ngsf_
             print(exc)
             pass
     else:
-        paths= glob.glob(f'/snid_api_results/pipeline_out/*/{target}/*h5')
+        paths= glob.glob(f'/snid_api_results/pipeline_out/*/{target[6:]}/*h5')
         try:
             auto_snid = f'{paths[0]}'
             try:
