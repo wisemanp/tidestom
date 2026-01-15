2934e4452b705da5f8759a9ad4106cd85da0fef4
Chris Duffy <c.jduffy@me.com>
2026-01-15 12:04:16 +0000

    Update settings.py

--- tidestom/settings.py
+++ tidestom/settings.py
@@ -224,8 +224,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
                     os.path.join(TEST_DIR, 'sims'),]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
 
 LOGGING = {
     'version': 1,
