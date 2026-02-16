604ae416f7751b3e58517b211a40804fe17d22d3
Tomás E. Müller Bravo <t.e.muller-bravo@tcd.ie>
2026-02-16 11:48:05 +0000

    better handling of Lasair API tokens (#191)
    This closes #183

--- myplots/templatetags/myplots_tags.py
+++ myplots/templatetags/myplots_tags.py
@@ -77,6 +77,7 @@ def target_spectroscopy(context, target, dataproduct=None, snid_path=None, snid_
             annotation_position="top",
             annotation_font=dict(size=12, color="black")
         )
+    
 
     ### arm joins ###
     # Inclusion of the 4MOST (low res) spectrograph arm overlap arm regions
@@ -156,7 +157,7 @@ def target_spectroscopy(context, target, dataproduct=None, snid_path=None, snid_
                              )
         except Exception as exc:
             return {'target': target, 'plot': f'<p>NGSF failed: {exc}</p>'}
-
+    
     fig.update_layout(autosize=True,
                       height=650,
                       xaxis_title='Observed Wavelength (Å)',
@@ -187,24 +188,28 @@ def target_photometry(context, target, dataproduct=None):
     Renders a photometry plot for a ``Target``. If a ``DataProduct`` is specified, it will only render a plot with
     that photometry.
     """
-    # check if the Lasair's API key is set
-    if lasair_ztf_token is None or lasair_ztf_token == "":
-        warnings.warn("Warning: Lasair API key for ZTF not set!", UserWarning)
-        return {'target': target}
-    if lasair_lsst_token is None or lasair_lsst_token == "":
-        warnings.warn("Warning: Lasair API key for LSST not set!", UserWarning)
-        return {'target': target}
-
+    tokens = {"ztf": lasair_ztf_token, 
+              "lsst": lasair_lsst_token, 
+              }
     photometry_list = []
-    for survey in ["ztf", "lsst"]:
-        #phot = fetch_target_lasair(49.1384664, 44.9725084, survey)  # ZTF25aacedrs for testing
+    for survey, token in tokens.items():
+        # check if the Lasair's API key is set
+        if token is None or token == "":
+            warnings.warn(f"Warning: Lasair API key for {survey.upper()} not set!", UserWarning)
+            continue
         try:
+            #phot = fetch_target_lasair(49.1384664, 44.9725084, survey)  # ZTF25aacedrs for testing
             phot = fetch_target_lasair(target.ra, target.dec, survey)
+            photometry_list.append(phot)
         except Exception as exc:
             return {'target': target, 'plot': exc}
-        photometry_list.append(phot)
+        
+    if len(photometry_list) == 0:
+        # tokens not set
+        return {'target': target}
     photometry = pd.concat(photometry_list)
     if photometry is None:
+        # no photometry found
         return {'target': target}
 
     # plot photometry
